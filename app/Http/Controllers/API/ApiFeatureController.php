04feac543c47d4a4e6f6fb268bba1b1279b26a07
kishormahmud9 <74523493+kishormahmud9@users.noreply.github.com>
2025-03-18 16:56:26 +0600

    update a new feature module

--- app/Http/Controllers/API/ApiFeatureController.php
+++ app/Http/Controllers/API/ApiFeatureController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Features;
 use Illuminate\Http\Request;
 
 class ApiFeatureController extends Controller
 {
-    //
+    public function viewAll()
+    {
+        $features = Features::get();
+
+        return response()->json([
+            'status' => 200,
+            'features' => $features
+        ]);
+    }
 }
