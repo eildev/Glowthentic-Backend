e540a41f498587e7eb4301a0c78afdded5893b26
Mdshahin12502077ms <md12502077ms@gmail.com>
2025-04-23 18:00:06 +0600

    data update 23/4

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -1,20 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login</title>
-</head>
-<body>
-    <h2>Admin Login</h2>
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <input type="email" name="email" placeholder="Email" required><br>
-        <input type="password" name="password" placeholder="Password" required><br>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html> --}}
 
 
 <!DOCTYPE html>
@@ -154,7 +137,7 @@
                         <small class="text-danger">{{ $message }}</small>
                     @enderror
                 </div>
-                <a class="btn btn-primary login">Login</a>
+                <button type="submit" class="btn btn-primary login">Login</button>
             </form>
 
         </div>
