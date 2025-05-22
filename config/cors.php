6385fb0335a6706147b53b11c7ff4328ee51e4c9
Fardus Hassan <webdev.fardus@gmail.com>
2025-05-22 12:13:27 +0600

    commit

--- config/cors.php
+++ config/cors.php
@@ -6,11 +6,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-<<<<<<< HEAD
     'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://127.0.0.1:8000', 'http://192.168.0.122:5173', 'http://192.168.0.191:5173'],
-=======
-    'allowed_origins' => ['*'],
->>>>>>> 75414530434d0b334584a91421549d2005394d2d
+    // 'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
