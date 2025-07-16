7f5d9ae24ac33234b037ce69649b475785a8976f
kishormahmud9 <74523493+kishormahmud9@users.noreply.github.com>
2025-07-16 15:41:16 +0600

    Product details page main work is complete

--- database/migrations/2024_01_11_050430_create_products_table.php
+++ database/migrations/2024_01_11_050430_create_products_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            // $table->unsignedBigInteger('category_id')->unsigned();
-            // $table->unsignedBigInteger('subcategory_id')->nullable();
+            $table->unsignedBigInteger('category_id')->unsigned();
+            $table->unsignedBigInteger('subcategory_id')->nullable();
             // $table->integer('sub_subcategory_id')->nullable();
             $table->unsignedBigInteger('brand_id')->unsigned();
             $table->json('product_feature')->nullable();
